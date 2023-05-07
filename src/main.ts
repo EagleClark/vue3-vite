4145b683f8233e762c2bb66e477f343337ae7c76
Eagle Ye <eagleclark@163.com>
2023-05-07 22:00:19 +0800

    feat: 增加自动导入和AntD

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
-import './style.css';
+import 'ant-design-vue/dist/antd.less';
+import './style.less';
 import App from './App.vue';
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+    AutoImport({
+      imports: ['vue'],
+    }),
+  ],
 });
